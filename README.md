d58a4324785c33989c0133d037f6316eab0df6fb
DominiqueMakowski <dom.mak19@gmail.com>
2016-11-25 00:48:43 +0100

    EEG

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# NeuroTools.py
-A Python Toolbox for Statistics and Signal Processing (EEG and bio)
+# NeuroTools
+A Python Toolbox for Statistics and Signal Processing (EEG and bio).
+
+---
+
+## Description
+
+Features:
+
+- EEG (based on [mne](http://martinos.org/mne/stable/index.html))
+- EDA
+
+## Install
+
+Run the following:
+
+```bash
+pip install https://github.com/neuropsychology/NeuroTools.py/zipball/master
+```
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+name = "neurotools",
+description = ("A Python Toolbox for Statistics and Signal Processing (EEG and bio)."),
+version = "0.0.1",
+license = "MIT",
+author = "Dominique Makowski",
+author_email = "dom.makowski@gmail.com",
+maintainer = "Dominique Makowski",
+maintainer_email = "dom.makowski@gmail.com",
+packages = find_packages(),
+package_data = {},
+install_requires = [
+    'numpy>=1.11.0',
+    'scipy>=0.10.0',
+    'pandas>=0.18.0',
+    'mne>=0.13.0',
+    'matplotlib',
+    'neuropsydia',
+    'nolds'],
+dependency_links=[],
+long_description = open('README.md').read(),
+keywords = "python signal processing EEG",
+url = "https://github.com/neuropsychology/NeuroTools.py",
+download_url = 'https://github.com/neuropsychology/NeuroTools.py/tarball/0.0.1',
+test_suite='nose.collector',
+tests_require=['nose'],
+classifiers = [
+	'Intended Audience :: Science/Research',
+	'Intended Audience :: Developers',
+	'Programming Language :: Python',
+	'Topic :: Software Development',
+	'Topic :: Scientific/Engineering',
+	'Operating System :: Microsoft :: Windows',
+	'Operating System :: Unix',
+	'Operating System :: MacOS',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6']
+)
